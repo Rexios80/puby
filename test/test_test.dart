ea830126877722e6bfd84450acaf7ee87e6dabbc
Rexios <rexios@rexios.dev>
2022-11-28 19:28:43 -0500

    Increase test timeout

--- test/test_test.dart
+++ test/test_test.dart
@@ -5,35 +5,39 @@ import 'package:test/test.dart';
 import 'test_utils.dart';
 
 void main() {
-  test('[engine] test', () async {
-    final result = await testCommand(['test', '--coverage']);
-    final stdout = result.stdout;
+  test(
+    '[engine] test',
+    () async {
+      final result = await testCommand(['test', '--coverage']);
+      final stdout = result.stdout;
 
-    // Since these projects have no tests, the command should fail
-    expect(result.exitCode, isNot(0));
+      // Since these projects have no tests, the command should fail
+      expect(result.exitCode, isNot(0));
 
-    // dart
-    expectLine(stdout, ['dart_puby_test', 'flutter test --coverage']);
-    // Explicit exclusion
-    expectLine(
-      stdout,
-      ['dart_puby_test${Platform.pathSeparator}example', 'Skip'],
-    );
+      // dart
+      expectLine(stdout, ['dart_puby_test', 'flutter test --coverage']);
+      // Explicit exclusion
+      expectLine(
+        stdout,
+        ['dart_puby_test${Platform.pathSeparator}example', 'Skip'],
+      );
 
-    // flutter
-    expectLine(stdout, ['flutter_puby_test', 'flutter test --coverage']);
-    // Explicit exclusion
-    expectLine(
-      stdout,
-      ['flutter_puby_test${Platform.pathSeparator}example', 'Skip'],
-    );
+      // flutter
+      expectLine(stdout, ['flutter_puby_test', 'flutter test --coverage']);
+      // Explicit exclusion
+      expectLine(
+        stdout,
+        ['flutter_puby_test${Platform.pathSeparator}example', 'Skip'],
+      );
 
-    // fvm
-    expectLine(stdout, ['fvm_puby_test', 'flutter test --coverage']);
-    // Explicit exclusion
-    expectLine(
-      stdout,
-      ['fvm_puby_test${Platform.pathSeparator}example', 'Skip'],
-    );
-  });
+      // fvm
+      expectLine(stdout, ['fvm_puby_test', 'flutter test --coverage']);
+      // Explicit exclusion
+      expectLine(
+        stdout,
+        ['fvm_puby_test${Platform.pathSeparator}example', 'Skip'],
+      );
+    },
+    timeout: Timeout.factor(1.5),
+  );
 }
