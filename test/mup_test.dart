d5302ba96a840caf43c4288ab5e03e6c901dd4f7
Rexios <rexios@rexios.dev>
2022-11-28 19:30:46 -0500

    ...

--- test/mup_test.dart
+++ test/mup_test.dart
@@ -5,64 +5,68 @@ import 'package:test/test.dart';
 import 'test_utils.dart';
 
 void main() {
-  test('[engine] mup', () async {
-    final result = await testCommand(['mup']);
-    final stdout = result.stdout;
+  test(
+    '[engine] mup',
+    () async {
+      final result = await testCommand(['mup']);
+      final stdout = result.stdout;
 
-    expect(result.exitCode, 0);
+      expect(result.exitCode, 0);
 
-    // dart
-    expectLine(stdout, [
-      'dart_puby_test',
-      '"dart pub upgrade --major-versions"',
-    ]);
-    expectLine(stdout, [
-      'dart_puby_test',
-      '"dart pub upgrade"',
-    ]);
-    expectLine(stdout, [
-      'dart_puby_test${Platform.pathSeparator}example',
-      '"dart pub upgrade --major-versions"',
-    ]);
-    expectLine(stdout, [
-      'dart_puby_test${Platform.pathSeparator}example',
-      '"dart pub upgrade"',
-    ]);
+      // dart
+      expectLine(stdout, [
+        'dart_puby_test',
+        '"dart pub upgrade --major-versions"',
+      ]);
+      expectLine(stdout, [
+        'dart_puby_test',
+        '"dart pub upgrade"',
+      ]);
+      expectLine(stdout, [
+        'dart_puby_test${Platform.pathSeparator}example',
+        '"dart pub upgrade --major-versions"',
+      ]);
+      expectLine(stdout, [
+        'dart_puby_test${Platform.pathSeparator}example',
+        '"dart pub upgrade"',
+      ]);
 
-    // flutter
-    expectLine(stdout, [
-      'flutter_puby_test',
-      '"flutter pub upgrade --major-versions"',
-    ]);
-    expectLine(stdout, [
-      'flutter_puby_test',
-      '"flutter pub upgrade"',
-    ]);
-    expectLine(stdout, [
-      'flutter_puby_test${Platform.pathSeparator}example',
-      '"flutter pub upgrade --major-versions"',
-    ]);
-    expectLine(stdout, [
-      'flutter_puby_test${Platform.pathSeparator}example',
-      '"flutter pub upgrade"',
-    ]);
+      // flutter
+      expectLine(stdout, [
+        'flutter_puby_test',
+        '"flutter pub upgrade --major-versions"',
+      ]);
+      expectLine(stdout, [
+        'flutter_puby_test',
+        '"flutter pub upgrade"',
+      ]);
+      expectLine(stdout, [
+        'flutter_puby_test${Platform.pathSeparator}example',
+        '"flutter pub upgrade --major-versions"',
+      ]);
+      expectLine(stdout, [
+        'flutter_puby_test${Platform.pathSeparator}example',
+        '"flutter pub upgrade"',
+      ]);
 
-    // fvm
-    expectLine(stdout, [
-      'fvm_puby_test',
-      '"fvm flutter pub upgrade --major-versions"',
-    ]);
-    expectLine(stdout, [
-      'fvm_puby_test',
-      '"fvm flutter pub upgrade"',
-    ]);
-    expectLine(stdout, [
-      'fvm_puby_test${Platform.pathSeparator}example',
-      '"fvm flutter pub upgrade --major-versions"',
-    ]);
-    expectLine(stdout, [
-      'fvm_puby_test${Platform.pathSeparator}example',
-      '"fvm flutter pub upgrade"',
-    ]);
-  });
+      // fvm
+      expectLine(stdout, [
+        'fvm_puby_test',
+        '"fvm flutter pub upgrade --major-versions"',
+      ]);
+      expectLine(stdout, [
+        'fvm_puby_test',
+        '"fvm flutter pub upgrade"',
+      ]);
+      expectLine(stdout, [
+        'fvm_puby_test${Platform.pathSeparator}example',
+        '"fvm flutter pub upgrade --major-versions"',
+      ]);
+      expectLine(stdout, [
+        'fvm_puby_test${Platform.pathSeparator}example',
+        '"fvm flutter pub upgrade"',
+      ]);
+    },
+    timeout: Timeout.factor(1.5),
+  );
 }
