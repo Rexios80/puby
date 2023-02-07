3a605404368de3077d5660106314a945651d780a
Rexios <rexios@rexios.dev>
2023-02-06 19:35:30 -0500

    Added test

---
+++ test/no_fvm_test.dart
@@ -0,0 +1,16 @@
+import 'package:test/test.dart';
+
+import 'test_utils.dart';
+
+void main() {
+  test('--no-fvm', () async {
+    final result = await testCommand(['get', '--no-fvm']);
+    final stdout = result.stdout;
+
+    expect(result.exitCode, 0);
+    expectLine(
+      stdout,
+      ['fvm_puby_test', 'Project uses FVM, but FVM support is disabled'],
+    );
+  });
+}
