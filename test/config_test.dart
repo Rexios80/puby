d606ab90d0d6ae33a8aa751eb70bec60be02501c
Rexios <rexios@rexios.dev>
2024-10-11 15:29:10 -0400

    Add config test

--- test/config_test.dart
+++ test/config_test.dart
@@ -1,6 +1,29 @@
+import 'package:io/io.dart';
 import 'package:test/test.dart';
 
 import 'test_utils.dart';
 import 'package:path/path.dart' as path;
 
-void main() {}
+void main() {
+  group('config', () {
+    test('exclude', () async {
+      final result = await testCommand(
+        ['gen'],
+        projects: {
+          'puby_yaml_test': {
+            'pubspec.yaml': pubspec('puby_yaml_test'),
+            'puby.yaml': '''
+exclude:
+  - pub run build_runner
+''',
+          },
+        },
+      );
+
+      // Since the code generation doesn't actually run the command should succeed
+      expect(result.exitCode, ExitCode.success.code);
+
+      expectLine(result, [path.join('puby_yaml_test'), 'Skip']);
+    });
+  });
+}
